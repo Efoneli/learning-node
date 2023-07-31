3f27fbc8fa4da478b2e481fbfca644bd15905702
efoneli <abbaefoneli@gmail.com>
2023-07-31 10:33:34 +0100

    node

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('<h1>Welcome to the first server in the world</h1>')
+});
+
+server.listen(4000, () => {
+    console.log('Server running on port 4000');
+})
--- index.js
+++
@@ -1,7 +0,0 @@
-let lang = 'JavaScript'
-
-console.log(lang)
-
-for(let i=0; i<=10; i++) {
-    console.log(i)
-}
