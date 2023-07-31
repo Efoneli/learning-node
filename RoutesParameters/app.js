750f6d2a69921abb37123c8b8863698aed101455
efoneli <abbaefoneli@gmail.com>
2023-07-31 17:17:30 +0100

    sorting routes

--- RoutesParameters/app.js
+++ RoutesParameters/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 
+// patreon routes for all questions
 app.get('/questions/:id/:title', (req, res) => {
     res.send('this route handles all the questions')
 });
 
+// patreon routes for all users
 app.get('/users/:id/:username', (req, res) => {
-    res.send('this route handles all the users of stackoverflow');
+    let userId = req.params.id;
+    let user = req.params.username;
+
+    res.send('This route handles '+user+' profile that has ID: '+userId);
 })
 
 app.listen(3000, () => {
