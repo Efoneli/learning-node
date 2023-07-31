6ca6c8c041f8557df094fc4ecab59b206581499c
efoneli <abbaefoneli@gmail.com>
2023-07-31 14:58:39 +0100

    express

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const http = require('http');
-const casual = require('casual'); 
+// const http = require('http');
+// const casual = require('casual'); 
 
-const server = http.createServer((req, res) => {
-    res.end(casual.city)
-});
-server.listen(4000, () => {
-    console.log('Server running on port 4000');
-})
+// const server = http.createServer((req, res) => {
+//     res.end(casual.city)
+// });
+// server.listen(4000, () => {
+//     console.log('Server running on port 4000');
+// })
+
+const names = [Christian, Esther, Lucky, Solomon, Sarah, Miracle]
