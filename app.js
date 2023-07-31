8a30bc9ef766cce8350b77e744e846a3b384946c
efoneli <abbaefoneli@gmail.com>
2023-07-31 11:46:34 +0100

    casual and nodemon

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('<h1>Welcome to the first server in the world</h1>')
+    res.end('<h1> another Welcome to the first server in the world</h1>')
 });
-
 server.listen(4000, () => {
     console.log('Server running on port 4000');
 })
