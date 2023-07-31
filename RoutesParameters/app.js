10a3be07baebf0d9f730ab1b2f4f952af7436525
efoneli <abbaefoneli@gmail.com>
2023-07-31 17:02:47 +0100

    working with routes

---
+++ RoutesParameters/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/questions/:id/:title', (req, res) => {
+    res.send('this route handles all the questions')
+});
+
+app.get('/users/:id/:username', (req, res) => {
+    res.send('this route handles all the users');
+})
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+})
