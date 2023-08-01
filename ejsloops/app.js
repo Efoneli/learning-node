9c13f6589d49120c3186f10b40d1bbfa99284767
efoneli <abbaefoneli@gmail.com>
2023-08-01 12:00:32 +0100

    ejs loops

---
+++ ejsloops/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
+
+app.get('/questions', (req, res) => {
+
+    let questions = [
+        {title: "What is Node.js", user: "John", votes: "10"},
+        {title: "What is MongoDB", user: "Paul", votes: "6"},
+        {title: "What is Express.js", user: "Mike", votes: "8"},
+    ]
+
+    res.render('index', {questions: questions});
+})
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+})
