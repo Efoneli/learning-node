a420e04b5d4f2777e36dd811a55dbed27d63cd17
efoneli <abbaefoneli@gmail.com>
2023-08-01 11:27:52 +0100

    linking stylesheet

--- firstEJSapp/app.js
+++ firstEJSapp/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 app.get('/user/:id/:username', (req, res) => {
     let userId = req.params.id;
     let user = req.params.username;
-    res.render('index.ejs', {id : userId, username : user})
+    res.render('index', {id : userId, username : user})
 })
 
 
